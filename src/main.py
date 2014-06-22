395443e7603a3f062bf9819c5609a52bfadc7da5
Yuta OHWATARI <yyutao88@gmail.com>
2014-06-22 11:18:33 +0900

    spec3: 愚直に実装

--- src/main.py
+++ src/main.py
@@ -2,4 +2,14 @@
 # -*- coding:utf-8 -*-
 
 if __name__ == "__main__":
-    print "オムライス"
+    # 読み込みデータ
+    recipe = []
+
+    # ファイル読み込み
+    with open("src/recipe.txt", 'rb') as fd:
+        for line in fd:
+            recipe.append(line)
+
+    # 出力
+    for line in recipe:
+        print line
