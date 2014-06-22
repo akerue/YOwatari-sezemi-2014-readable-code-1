3f174b0bfd001533ecf2838a43ea61787f33b68d
Yuta OHWATARI <yyutao88@gmail.com>
2014-06-22 11:46:47 +0900

    spec6: Recipeクラスを作成

--- src/main.py
+++ src/main.py
@@ -6,6 +6,17 @@ import sys
 argv = sys.argv
 argc = len(sys.argv)
 
+class Recipe:
+
+    def __init__(self, filepath):
+        self.data = []
+
+        # ファイル読み込み
+        with open(filepath, 'rb') as fd:
+            for line in fd:
+                self.data.append(line.rstrip())  # 改行を除去して追加
+
+
 if __name__ == "__main__":
     filepath = ""  # 引数で受け取るファイルパス
     recipe = []    # 読み込みデータ
@@ -17,12 +28,8 @@ if __name__ == "__main__":
         print "引数が不正です。"
         quit()
 
-    # ファイル読み込み
-    with open(filepath, 'rb') as fd:
-        for line in fd:
-            recipe.append(line)
+    recipe = Recipe(filepath)
 
     # 出力
-    for recipe_id, line in enumerate(recipe):
-        recipe_name = line.rstrip()  # 改行除去
+    for recipe_id, recipe_name in enumerate(recipe.data):
         print "%d: %s" % (recipe_id, recipe_name)
