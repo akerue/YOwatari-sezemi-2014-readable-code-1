d4cd52558958ddf82fb9f641b0e183e5eb80d752
akerue <hsmgn8118akerue@gmail.com>
2014-06-22 14:36:09 +0900

    Add function to display user name

--- src/main.py
+++ src/main.py
@@ -2,6 +2,7 @@
 # -*- coding:utf-8 -*-
 
 import sys
+import codecs
 
 
 argv = sys.argv
@@ -27,31 +28,44 @@ class Recipe:
 
         # ファイル読み込み
         tmp_data = []
-        with open(filepath, 'rb') as fd:
-            for line in fd:
+        with codecs.open(filepath, 'r', encoding="utf-8") as fd:
+            lines = fd.readlines()
+            self.user = lines.pop(0).rstrip() # 最初の行はユーザ名
+            for line in lines:
                 # 改行を除去して名前とURLにパース
                 name, url = line.rstrip().split(" ")
                 tmp_data.append(Dish(name, url))
         self.dic = list2dict(tmp_data)
 
+def print_recipe(recipe, recipe_id=None):
+
+    print "%s: " % (recipe.user)
+
+    if recipe_id is None:
+        # ID指定なし
+
+        for recipe_id, dish in recipe.dic.items():
+            print "%d: %s" % (recipe_id, dish.get_info_str())
+    else:
+        # ID指定あり
+        if output_id in recipe.dic:
+            print "%d: %s" % (output_id, recipe.dic[output_id].get_info_str())
+        else:
+            print "レシピがありません。"
 
 if __name__ == "__main__":
     # 引数を念のためチェック
     if argc == 2:
         recipe = Recipe(filepath=argv[1])
 
-        # 出力
-        for recipe_id, dish in recipe.dic.items():
-            print "%d: %s" % (recipe_id, dish.get_info_str())
+        print_recipe(recipe)
+
     elif argc == 3:
         recipe = Recipe(filepath=argv[1])
         output_id = int(argv[2])
 
-        # 出力
-        if output_id in recipe.dic:
-            print "%d: %s" % (output_id, recipe.dic[output_id])
-        else:
-            print "レシピがありません。"
+        print_recipe(recipe, output_id)
+
     else:
         print "引数が不正です。"
         quit()
