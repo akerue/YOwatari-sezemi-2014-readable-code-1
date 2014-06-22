6d66de79b976a71463948f3803eb20722bea58b0
akerue <hsmgn8118akerue@gmail.com>
2014-06-22 14:07:44 +0900

    spec7: レシピ情報にURLを追加

--- src/main.py
+++ src/main.py
@@ -11,38 +11,45 @@ argc = len(sys.argv)
 def list2dict(val_list):
     return dict(zip(range(0,len(val_list)), val_list))
 
+class Dish:
+
+    def __init__(self, name, url):
+        self.name = name
+        self.url = url
+
+    def get_info_str(self):
+        return "%s %s" % (self.name, self.url) # 料理の情報文字列を返す
 
 class Recipe:
 
     def __init__(self, filepath):
-        self.data = {}
+        self.dic = {}
 
         # ファイル読み込み
         tmp_data = []
         with open(filepath, 'rb') as fd:
             for line in fd:
-                tmp_data.append(line.rstrip())  # 改行を除去して追加
-        self.data = list2dict(tmp_data)
+                # 改行を除去して名前とURLにパース
+                name, url = line.rstrip().split(" ")
+                tmp_data.append(Dish(name, url))
+        self.dic = list2dict(tmp_data)
 
 
 if __name__ == "__main__":
-    filepath = ""  # 引数で受け取るファイルパス
-    recipe = []    # 読み込みデータ
-
     # 引数を念のためチェック
     if argc == 2:
         recipe = Recipe(filepath=argv[1])
 
         # 出力
-        for recipe_id, recipe_name in recipe.data.items():
-            print "%d: %s" % (recipe_id, recipe_name)
+        for recipe_id, dish in recipe.dic.items():
+            print "%d: %s" % (recipe_id, dish.get_info_str())
     elif argc == 3:
         recipe = Recipe(filepath=argv[1])
         output_id = int(argv[2])
 
         # 出力
-        if output_id in recipe.data:
-            print "%d: %s" % (output_id, recipe.data[output_id])
+        if output_id in recipe.dic:
+            print "%d: %s" % (output_id, recipe.dic[output_id])
         else:
             print "レシピがありません。"
     else:
