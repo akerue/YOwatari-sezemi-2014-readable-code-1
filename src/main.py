6cba31a1d5049343d440d1c65511dd07f7522215
Yuta OHWATARI <yyutao88@gmail.com>
2014-06-22 12:00:47 +0900

    spec6: レシピデータをリストから辞書で扱うよう変更

--- src/main.py
+++ src/main.py
@@ -3,18 +3,26 @@
 
 import sys
 
+
 argv = sys.argv
 argc = len(sys.argv)
+
+# keyがlistの添字な辞書へ変換
+def list2dict(val_list):
+    return dict(zip(range(0,len(val_list)), val_list))
+
 
 class Recipe:
 
     def __init__(self, filepath):
-        self.data = []
+        self.data = {}
 
         # ファイル読み込み
+        tmp_data = []
         with open(filepath, 'rb') as fd:
             for line in fd:
-                self.data.append(line.rstrip())  # 改行を除去して追加
+                tmp_data.append(line.rstrip())  # 改行を除去して追加
+        self.data = list2dict(tmp_data)
 
 
 if __name__ == "__main__":
@@ -23,13 +31,12 @@ if __name__ == "__main__":
 
     # 引数を念のためチェック
     if argc == 2:
-        filepath = argv[1]
+        recipe = Recipe(filepath=argv[1])
     else:
         print "引数が不正です。"
         quit()
 
-    recipe = Recipe(filepath)
 
     # 出力
-    for recipe_id, recipe_name in enumerate(recipe.data):
+    for recipe_id, recipe_name in recipe.data.items():
         print "%d: %s" % (recipe_id, recipe_name)
